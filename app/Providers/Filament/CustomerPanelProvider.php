30c6c06beffd2799d14b190601007557c6200325
Adeoti Nurudeen <adeoti360@gmail.com>
2024-06-18 19:45:01 +0100

    Second PushBCDE

--- app/Providers/Filament/CustomerPanelProvider.php
+++ app/Providers/Filament/CustomerPanelProvider.php
@@ -53,7 +53,7 @@ class CustomerPanelProvider extends PanelProvider
             //->brandLogoHeight('2rem')
             
             ->colors([
-                'primary' => Color::Indigo,
+                'primary' => Color::Emerald,
                 'danger' => Color::Rose,
                 'gray' => '#000000', //Color::Gray,
                 'info' => Color::Blue,
